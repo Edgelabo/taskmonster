5eca707e7a16d159e8c1f31429e1d7e342cf2270
Ekkun <harunia2020@gmai.com>
2020-09-24 23:41:24 +0900

    Release to heroku

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  #nameカラム空欄禁止
+  validates :name, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'todoes#index'
+  devise_for :users, controllers: {
+      sessions: 'users/registrations',
+      sessions: 'users/sessions'
+  }
+
 end
