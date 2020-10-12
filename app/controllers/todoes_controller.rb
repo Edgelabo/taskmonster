fc917d60dd123c2235ad34d840e52cf44c5df1c9
Ekkun <edge@nire.com>
2020-10-13 02:40:27 +0900

    save

--- app/controllers/todoes_controller.rb
+++ app/controllers/todoes_controller.rb
@@ -7,30 +7,29 @@ class TodoesController < ApplicationController
       redirect_to controller: 'todoes', action: 'show', id: current_user.id
     end
   end
-
-  @@current_goal_id = 0
-  @@goal = 0
+  #@@current_goal_id = 0
+  # @@goal = 0
   def show
     @user = User.find(params[:id])
     @goal = @user.goals.last
-    @@goal = @user.goals.last #タスク完了にわたすため
+    # @@goal = @user.goals.last #タスク完了にわたすため
     #初回は目標を設定させる
     if (@goal.nil?)
       redirect_to controller: 'todoes', action: 'new'
       return
     end
     if !(@goal.nil?)
-      @@current_goal_id = @goal.id
+      #@@current_goal_id = @goal.id
       @current_monster_id = @goal.monster_id
     end
     @monster = Monster.find(@current_monster_id)
     @task = Task.new
     # 完了、未完了タスク
-    @get_check = Task.where(goal_id: @@current_goal_id).where(check: true)
-    @get_uncheck = Task.where(goal_id: @@current_goal_id).where(check: false)
+    @get_check = Task.where(goal_id: @goal.id).where(check: true)
+    @get_uncheck = Task.where(goal_id: @goal.id).where(check: false)
     # タスク完了、削除
-    @check_task = Task.find_by(goal_id: @@current_goal_id)
-    @destroy_task = Task.find_by(goal_id: @@current_goal_id)
+    @check_task = Task.find_by(goal_id: @goal.id)
+    @destroy_task = Task.find_by(goal_id: @goal.id)
     # モンスターコメント
     comment_max = Encourage.count;
     @comment = Encourage.find(rand(1..comment_max)).comment
@@ -51,7 +50,8 @@ class TodoesController < ApplicationController
   #タスク作成
   def task_create
     @task = Task.new(task_params)
-    @task.goal_id = @@current_goal_id
+    #@task.goal_id = @@current_goal_id
+    #@task.goal_id = id_params
     @task.save
 
     redirect_to controller: 'todoes', action: 'show', id: current_user.id
@@ -61,13 +61,14 @@ class TodoesController < ApplicationController
     @task = Task.find(params[:id])
     @task.update(check: true)
     #経験値追加
-    @@goal.increment(:ex , 10)
-    @@goal.save
+    @goal = current_user.goals.last
+    @goal.increment(:ex , 10)
+    @goal.save
 
-    if @@goal.ex >= 30
-       @@goal.increment(:lv ,1)
-       @@goal.save
-       @@goal.update(ex: 0)
+    if @goal.ex >= 30
+       @goal.increment(:lv ,1)
+       @goal.save
+       @goal.update(ex: 0)
      end
     redirect_to controller: 'todoes', action: 'show', id: current_user.id
   end
@@ -80,6 +81,6 @@ class TodoesController < ApplicationController
     params.require(:goal).permit(:goal)
   end
   def task_params
-    params.require(:task).permit(:task)
+    params.require(:task).permit(:task,:goal_id)
   end
 end
